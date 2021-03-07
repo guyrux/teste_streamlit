e2df7f2f65b01564220fb00111eb6a41c3c6050b
Gustavo Suto <guyrux@gmail.com>
2021-03-07 14:50:15 -0300

    [chore] Added the travis and flake8 files.

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
+# import numpy as np
+# import pandas as pd
+
 
 def main():
-    page = st.sidebar.selectbox("Escolha uma opção:", ['Opção 1', 'Opção 2', 'Opção 3'])
+    st.sidebar.selectbox("Escolha uma opção:", ['Opção 1', 'Opção 2', 'Opção 3'])
     st.header('Tutorial Streamlit - Heroku')
     st.write('Esta é uma página de teste.')
+
 
 if __name__ == '__main__':
     main()
